2142dcee91ca7e6414d838c02209435d51f54470
Plextora <71889427+Plextora@users.noreply.github.com>
2023-02-26 13:16:39 -0500

    Added a option selection menu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,48 @@
-﻿namespace LatiteMinimal
+﻿using System;
+
+namespace LatiteMinimal
 {
     internal class Program
     {
+        public static void WriteColor(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
         public static void Main(string[] args)
         {
+            Console.Clear();
+            WriteColor("Do you want to use Latite Client or a custom DLL?", ConsoleColor.White);
+            Console.WriteLine("[1] Latite Client");
+            Console.WriteLine("[2] Custom DLL");
+            Console.WriteLine("[3] Exit\n");
             
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("> ");
+                Console.ResetColor();
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        WriteColor(
+                            "Invalid option! (Example option selection: Enter 1 for the \"[1] Latite Client\" option)",
+                            ConsoleColor.Red);
+                        continue;
+                }
+
+                break;
+            }
         }
     }
 }
